59bd3d75125a0806e5df8aad104c9380897dc820
Eetu Mäkelä <eetu.makela@iki.fi>
2020-05-06 15:11:35 +0300

    fix ports

--- dispatcher/dispatcher.py
+++
@@ -1,5 +0,0 @@
-from gensim.models import lda_dispatcher
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-lda_dispatcher.main()
--- worker/worker.py
+++
@@ -1,5 +0,0 @@
-from gensim.models import lda_worker
-import logging
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-lda_worker.main()
